4907ba2544e171b1a70e848447c64cd436299744
Sara <sara.krieger2022@gmail.com>
2024-01-24 20:23:25 +0100

    Add remove exclamation mark bla2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Ursula???");
-        System.out.println("Hello team");
+        System.out.println("Hello Frankie");
         System.out.println("Hello Sara");
         System.out.println("Hello Barbara");
     }
