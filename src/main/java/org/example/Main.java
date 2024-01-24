e7885ffc06ea1bd5e06567e5eedd5fb1c865ddf7
skychili <108395672+skychili@users.noreply.github.com>
2024-01-24 20:19:26 +0100

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Sara???");
+        System.out.println("Hello Ursula???");
         System.out.println("Hello team");
         System.out.println("Hello Sara");
         System.out.println("Hello Barbara");
